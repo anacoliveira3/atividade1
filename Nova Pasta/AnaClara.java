cd27de0cb50f51a87f8b5c83a66a07d40bc167ff
anacoliveira3 <ana.cardoso@alunos.ifsuldeminas.edu.br>
2020-11-18 15:17:44 -0300

    Carga Inicial

--- Nova Pasta/AnaClara.java
+++ Nova Pasta/AnaClara.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author bahia
- */
 public class AnaClara {
     
+    public static void main(String[] args) {
+        
+        System.out.println("Ana Clara");
+        
+    }
 }
