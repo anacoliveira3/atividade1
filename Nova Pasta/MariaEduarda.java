a39499b9791fde927c23ee360cc2c2ba2f7caa5b
anacoliveira3 <ana.cardoso@alunos.ifsuldeminas.edu.br>
2020-11-22 20:22:52 -0300

    Preenchendo classe, Ana Clara

--- Nova Pasta/MariaEduarda.java
+++ Nova Pasta/MariaEduarda.java
@@ -3,6 +3,8 @@ public class MariaEduarda {
     public static void main(String[] args) {
         
         System.out.println("Maria Eduarda");
+        
+        System.out.println("Ana Clara");
    
     }
 }
