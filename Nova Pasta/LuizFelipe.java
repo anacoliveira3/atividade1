cd64e3d9d9b7b9cb95cde7dce2b5a9c6790e1a00
anacoliveira3 <ana.cardoso@alunos.ifsuldeminas.edu.br>
2020-11-23 12:53:06 -0300

    Preenchendo classe, Ana Clara

--- Nova Pasta/LuizFelipe.java
+++ Nova Pasta/LuizFelipe.java
@@ -3,5 +3,7 @@ public class LuizFelipe {
     public static void main(String[] args) {
         
         System.out.println("Luiz Felipe");
+        
+        System.out.println("Ana Clara");
     }
 }
